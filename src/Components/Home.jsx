2b4cba45a7995e4648cf7ca7f8a2ba08df6c4fea
ingole8888 <ingole8888@gmail.com>
2022-07-20 22:26:53 +0530

    body page added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import IndexBody from "./IndexBody"
 export default function Home(){
     return(
         <>
         <Navbar/>
         <Footer/>
+        <IndexBody/>
         </>
     )
 }
---
+++ src/Components/IndexBody.jsx
@@ -0,0 +1,9 @@
+
+
+export default function IndexBody(){
+    return(
+        <>
+        
+        </>
+    )
+}
