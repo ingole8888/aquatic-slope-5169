04f1d67ca0c66edd255c84376f3b1c4beaf90b24
ingole8888 <ingole8888@gmail.com>
2022-07-21 18:15:46 +0530

    sign up login Authentication complete

--- src/App.js
+++ src/App.js
@@ -11,3 +11,42 @@ function App() {
 }
 
 export default App;
+// import { Container, Row, Col } from "react-bootstrap";
+// import { Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import Navbar from "./Components/Navbar"
+// import Home from "./Components/Home";
+// import Login from "./Components/Login";
+// import Signup from "./Components/Signup";
+// import ProtectedRoute from "./Components/ProtectedRoute";
+// import { UserAuthContextProvider } from "./context/UserAuthContext";
+
+// function App() {
+//   return (
+//     <>
+//     <Navbar/>
+//     <Container style={{ width: "400px" }}>
+//       <Row>
+//         <Col>
+//           <UserAuthContextProvider>
+//             <Routes>
+//               <Route
+//                 path="/home"
+//                 element={
+//                   <ProtectedRoute>
+//                     <Home />
+//                   </ProtectedRoute>
+//                 }
+//               />
+//               <Route path="/" element={<Login />} />
+//               <Route path="/signup" element={<Signup />} />
+//             </Routes>
+//           </UserAuthContextProvider>
+//         </Col>
+//       </Row>
+//     </Container>
+//     </>
+//   );
+// }
+
+// export default App;
