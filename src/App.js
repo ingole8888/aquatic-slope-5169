6ba43a325c75787091cc690259c68c21679c5a11
ingole8888 <ingole8888@gmail.com>
2022-07-23 11:18:57 +0530

    product page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
+import Reciepes from "./Components/Reciepes";
 import Signup from "./Components/Signup";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 
@@ -13,6 +14,7 @@ function App() {
               <Route path="/" element={<Home />}/>
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
+              <Route path="/reciepes" element={<Reciepes />} />
             </Routes>
           </UserAuthContextProvider>
     </div>
---
+++ src/Components/Reciepes.js
@@ -0,0 +1,18 @@
+import "./Reciepes.css";
+import  Navbar  from "./Navbar";
+import Footer from "./Footer"
+
+export default function Reciepes(){
+    return(
+        <>
+        <Navbar/>
+
+        {/* reciepes start */}
+        
+
+
+        {/* reciepes end */}
+        <Footer/>
+        </>
+    )
+}
